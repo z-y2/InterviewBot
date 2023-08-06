7491f97c38041d49c9becc6cad11378d69a6fe83
Ye <zhaye125@gmail.com>
2023-08-06 16:35:04 -0400

    new files

--- project/app/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class React(models.Model):
-    # CHANGE THIS FIELD LATER ########
-    employee = models.CharField(max_length=30)
-    department = models.CharField(max_length=200)
-    
